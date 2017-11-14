e5ce8bba77952944c4386fd64268dcbfc311383b
Liz Lee <lizleeux@gmail.com>
2017-11-14 12:04:18 -0800

    changed landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -12,11 +12,28 @@ var animatePoints = function(points) {
 
  };
 
-document.getElementsByClassName('hero-title')[0].onclick = function() {changeGreen()};
+// document.getElementsByClassName('hero-title')[0].onclick = function() {changeBackground()};
+//
+//  function changeBackground() {
+//      document.getElementsByClassName('hero-title')[0].style.backgroundColor = "white";
+//  }
 
- function changeGreen() {
-     document.getElementsByClassName('hero-title')[0].style.backgroundColor = "white";
- }
+// select element and set it to a variable
+var heroTitle = document.getElementsByClassName('hero-title')[0];
+
+// make a function to do what we want to do
+var clickChangeBackground = function() {
+   heroTitle.style.backgroundColor = "white";
+};
+
+// run the function on the hero title on click
+heroTitle.addEventListener("click", clickChangeBackground);
+// 
+// heroTitle.onclick = function() {
+//     clickChangeBackground();
+// };
+
+
  window.onload = function() {
      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
      if (window.innerHeight > 950) {
