4b69864245755e92882ad29a871590a64901b72b
Liz Lee <lizleeux@gmail.com>
2017-11-16 19:10:32 -0800

    Checkpoint 28 Jquery: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,49 +1,28 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
 
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function() {
+             // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
 };
-
-var animatePoints = function(points) {
-    forEach(points, revealPoint);
+//#6
+    $.each($('.point'), revealPoint);
 
  };
 
-// document.getElementsByClassName('hero-title')[0].onclick = function() {changeBackground()};
-//
-//  function changeBackground() {
-//      document.getElementsByClassName('hero-title')[0].style.backgroundColor = "white";
-//  }
-
-// select element and set it to a variable
-// var heroTitle = document.getElementsByClassName('hero-title')[0];
-//
-// // make a function to do what we want to do
-// var clickChangeBackground = function() {
-//    heroTitle.style.backgroundColor = "white";
-// };
-//
-// // run the function on the hero title on click
-// heroTitle.addEventListener("click", clickChangeBackground);
-//
-// heroTitle.onclick = function() {
-//     clickChangeBackground();
-// };
-
-
- window.onload = function() {
+ $(window).load(function() {
      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+     if ($(window).height() > 950) {
+         animatePoints();
+}
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
 
-     window.addEventListener('scroll', function(event) {
-         if (pointsArray[0].getBoundingClientRect().top <= 500) {
-              animatePoints(pointsArray);
+     $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
           }
       });
- }
+});
